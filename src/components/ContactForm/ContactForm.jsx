2c224cf6b21d79eeb64e7cd04c5af0711ebc5250
MicRedd1985 <micredd1985@gmail.com>
2023-01-19 14:30:53 +0200

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
+
 import { Form, Button } from './ContactForm.styled';
 
 export default function ContactForm({ onSubmit }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-
-  const nameId = nanoid();
-  const phoneId = nanoid();
 
   const handleChangeName = evt => {
     setName(evt.target.value);
@@ -37,7 +34,6 @@ export default function ContactForm({ onSubmit }) {
           type="text"
           name="name"
           onChange={handleChangeName}
-          id={nameId}
           value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -50,7 +46,6 @@ export default function ContactForm({ onSubmit }) {
           type="tel"
           name="number"
           onChange={handleChangeNumber}
-          id={phoneId}
           value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
